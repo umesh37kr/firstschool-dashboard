f8e48fe2f7448704b7bdedd38160059a3d554c21
umesh <umesh37kr@gmail.com>
2024-12-02 16:22:52 +0530

    setup shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
       <h1>welcome to school dashboard</h1>
+      <Button variant={"outline"}>click me</Button>
     </>
   );
 }

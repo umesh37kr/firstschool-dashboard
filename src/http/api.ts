854fa632e6e84f140d1c7c96a506b72ff3cf4319
umesh <umesh37kr@gmail.com>
2024-12-06 17:49:07 +0530

    add login functionality

---
+++ src/http/api.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:4000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const login = async (data: { username: string; password: string }) => {
+  return api.post("/api/user/login", data);
+};
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,17 +8,32 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { login } from "@/http/api";
+import { useMutation } from "@tanstack/react-query";
 import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const handleLoginSubmit = () => {
-    const email = emailRef.current?.value;
-    const password = passwordRef.current?.value;
-    console.log("data:", email, password);
-  };
+  const navigate = useNavigate();
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      navigate("/dashboard/home");
+      console.log("login successfull");
+    },
+  });
+  const handleLoginSubmit = () => {
+    const username = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    if (!username || !password) {
+      return alert("please enter email id and password");
+    }
+    // server call
+    mutation.mutate({ username, password });
+  };
   return (
     <section className="flex items-center justify-center h-screen">
       <Card className="mx-auto max-w-sm">

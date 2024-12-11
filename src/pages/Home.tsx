93a0a79c5a154ab1ff885561bce1252b2e27d0f4
umesh <umesh37kr@gmail.com>
2024-12-11 19:36:08 +0530

    add breadcrumb to dashboard

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,30 @@
 import { Button } from "@/components/ui/button";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink>
+              <Link to="/dashboard/home">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Home</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <div className="flex items-center">
         <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
       </div>
--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -8,10 +8,19 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { studentList } from "@/http/api";
 import { Students } from "@/types";
 import { useQuery } from "@tanstack/react-query";
 import { CircleUserRound } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Student = () => {
   const { isLoading, isError, data, error } = useQuery({
@@ -26,53 +35,69 @@ const Student = () => {
   console.log(data);
   const students = data?.data.students;
   return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Avatar</TableHead>
-          <TableHead>Roll No.</TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead>Class</TableHead>
-          <TableHead>Section</TableHead>
-          <TableHead>Gender</TableHead>
-          <TableHead>Date Of Birth</TableHead>
-          <TableHead>Created at</TableHead>
-          <TableHead>Address</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {students.map((student: Students) => (
-          <TableRow key={student._id}>
-            <TableCell>
-              {student.avatar ? (
-                <img
-                  src={student.avatar}
-                  alt={student.firstName}
-                  className="h-10 w-10 rounded-full"
-                />
-              ) : (
-                <CircleUserRound />
-              )}
-            </TableCell>
-            <TableCell>{student.rollNumber}</TableCell>
-            <TableCell>{student.firstName}</TableCell>
-            <TableCell>{student.classes}</TableCell>
-            <TableCell>{student.section}</TableCell>
-            <TableCell>{student.gender}</TableCell>
-            <TableCell>{student.dateOfBirth}</TableCell>
-            <TableCell>{student.createdAt}</TableCell>
-            <TableCell>{student.address}</TableCell>
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink>
+              <Link to="/dashboard/home">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Student</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Avatar</TableHead>
+            <TableHead>Roll No.</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Class</TableHead>
+            <TableHead>Section</TableHead>
+            <TableHead>Gender</TableHead>
+            <TableHead>Date Of Birth</TableHead>
+            <TableHead>Created at</TableHead>
+            <TableHead>Address</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-      <TableFooter>
-        <TableRow>
-          <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell>
-        </TableRow>
-      </TableFooter>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {students.map((student: Students) => (
+            <TableRow key={student._id}>
+              <TableCell>
+                {student.avatar ? (
+                  <img
+                    src={student.avatar}
+                    alt={student.firstName}
+                    className="h-10 w-10 rounded-full"
+                  />
+                ) : (
+                  <CircleUserRound />
+                )}
+              </TableCell>
+              <TableCell>{student.rollNumber}</TableCell>
+              <TableCell>{student.firstName}</TableCell>
+              <TableCell>{student.classes}</TableCell>
+              <TableCell>{student.section}</TableCell>
+              <TableCell>{student.gender}</TableCell>
+              <TableCell>{student.dateOfBirth}</TableCell>
+              <TableCell>{student.createdAt}</TableCell>
+              <TableCell>{student.address}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableCell colSpan={3}>Total</TableCell>
+            <TableCell className="text-right">$2,500.00</TableCell>
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </>
   );
 };
 

eee3b06652e473623ad45e3a0967458146b5d9aa
umesh <umesh37kr@gmail.com>
2025-01-01 22:00:48 +0530

    remove table footer

--- src/pages/Student.tsx
+++ src/pages/Student.tsx
@@ -110,12 +110,6 @@ const Student = () => {
             </TableRow>
           ))}
         </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell colSpan={3}>Total</TableCell>
-            <TableCell className="text-right">$2,500.00</TableCell>
-          </TableRow>
-        </TableFooter>
       </Table>
     </>
   );

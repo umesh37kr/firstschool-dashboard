18d158ebc69abba7532393f2d3333ea2084d103e
umesh <umesh37kr@gmail.com>
2025-01-06 20:20:27 +0530

    add edit dialog box for notice

--- src/pages/NoticeBoard.tsx
+++ src/pages/NoticeBoard.tsx
@@ -31,12 +31,37 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+// import { useMediaQuery } from "@/hooks/use-media-query"
+import { useMediaQuery } from "@react-hook/media-query";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
 import { useToast } from "@/hooks/use-toast";
 import { AxiosError } from "axios";
 import { ErrorResponse } from "@/types";
 
 const NoticeBoard = () => {
+  const [open, setOpen] = React.useState(false);
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   const { toast } = useToast();
   const [openRow, setOpenRow] = React.useState<string | null>(null);
   const queryClient = useQueryClient();
@@ -79,6 +104,10 @@ const NoticeBoard = () => {
   const deleteHandler = (id: string) => {
     deleteMutation.mutate(id);
   };
+  function test() {
+    alert(setOpen);
+  }
+
   return (
     <>
       <div>
@@ -94,6 +123,38 @@ const NoticeBoard = () => {
           </BreadcrumbList>
         </Breadcrumb>
       </div>
+      {/* drawer form for editing notice */}
+      {isDesktop && (
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <ProfileForm />
+          </DialogContent>
+        </Dialog>
+      )}
+      {!isDesktop && (
+        <Drawer open={open} onOpenChange={setOpen}>
+          <DrawerContent>
+            <DrawerHeader className="text-left">
+              <DrawerTitle>Edit profile</DrawerTitle>
+              <DrawerDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DrawerDescription>
+            </DrawerHeader>
+            <ProfileForm className="px-4" />
+            <DrawerFooter className="pt-2">
+              <DrawerClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      )}
       <Table>
         <TableCaption>A list of recent messages.</TableCaption>
         <TableHeader>
@@ -122,7 +183,14 @@ const NoticeBoard = () => {
                   <DropdownMenuContent align="end" className="w-[200px]">
                     <DropdownMenuLabel>Actions</DropdownMenuLabel>
                     <DropdownMenuGroup>
-                      <DropdownMenuItem>Edit</DropdownMenuItem>
+                      <DropdownMenuItem
+                        onClick={() => {
+                          setOpen(true); // Open the Drawer
+                          setOpenRow(null); // Close the DropdownMenu
+                        }}
+                      >
+                        Edit
+                      </DropdownMenuItem>
                       <DropdownMenuSeparator />
                       <DropdownMenuSeparator />
                       <DropdownMenuItem
@@ -142,5 +210,21 @@ const NoticeBoard = () => {
     </>
   );
 };
+
+function ProfileForm({ className }: React.ComponentProps<"form">) {
+  return (
+    <form className={cn("grid items-start gap-4", className)}>
+      <div className="grid gap-2">
+        <Label htmlFor="email">Email</Label>
+        <Input type="email" id="email" defaultValue="shadcn@example.com" />
+      </div>
+      <div className="grid gap-2">
+        <Label htmlFor="username">Username</Label>
+        <Input id="username" defaultValue="@shadcn" />
+      </div>
+      <Button type="submit">Save changes</Button>
+    </form>
+  );
+}
 
 export default NoticeBoard;

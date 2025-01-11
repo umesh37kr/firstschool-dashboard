4ce383035c28c67ab2939317f25fc37f89dd4a7f
umesh <umesh37kr@gmail.com>
2025-01-11 15:54:25 +0530

    add student number to admin dashboard

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -237,7 +237,7 @@ const DashboardLayout = () => {
             </DropdownMenuContent>
           </DropdownMenu>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-gray-50">
           <Outlet />
         </main>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,8 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Card } from "@/components/ui/card";
+import { FaUserGroup } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
@@ -24,8 +26,17 @@ const HomePage = () => {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      <div className="flex items-center">
-        <h1 className="text-lg font-semibold md:text-2xl">Dashboard</h1>
+      <div className="">
+        <Card className="w-[200px] h-[100px] flex items-center justify-around bg-gray-50">
+          <div className="w-20 h-20 rounded-full border-2 bg-green-200 flex justify-center items-center">
+            <FaUserGroup color="#41c353" size={"30px"} />
+          </div>
+
+          <div>
+            <h1 className="text-gray-500 font-medium">Student</h1>
+            <h1 className="text-gray-800 text-xl font-semibold">1200</h1>
+          </div>
+        </Card>
       </div>
     </>
   );
